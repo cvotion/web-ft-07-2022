bc9a051a951c7ed10d93d47312adbe321930aa04
Christian Votion <christianvotion@gmail.com>
2022-07-06 13:35:46 -0500

    added formatting lab work

--- lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
+++ lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
@@ -1,27 +1,41 @@
 
 # Create a variable called first_name and set the value to your first name
 
+first_name = "Christian"
+
 # create a variable called last_name and set the value to your last name
+
+last_name = "Votion"
 
 # print your first name console
 
+print(first_name)
+
 # print your last name to the console
+
+print(last_name)
 
 # Create a new varaiable called my_name and set the value to the concatenation of your first name and your last name 
 
+my_name = (first_name + " " + last_name)
+
 # print "My name is ???" out to the console.  Substitue the ??? with the my_name variable.
+
+print("My name is", my_name)
 
 # create a variable called company_name and set the value to "DigitalCrafts"
 
+company_name = "DigitalCrafts"
 
 # Using the two variables you set earlier, generate a company email address that follows the pattern: "first name, period, last name @ company domain". Assign the result to a new variable called email and print the email to the console.
 # harry.potter@DigitalCrafts.com
 
-
+print(f"{first_name}.{last_name}@{company_name}.com")
 
 # Using the firstname and lastname variables, generate a username to a new variable and print it to the console. The username should follow this format: first name, underscore, lastname.
 # harry_potter
 
+print(f"{first_name}_{last_name}")
 
 # Using all the information you have created, print the data in a nice format. That is, generate a "Contact Card" style output that looks better than just printing values.
 
@@ -29,6 +43,8 @@
 # Email: harry.potter@DigitalCrafts.com
 # Username: harry_potter
 
-
+print(f"Welcome to {company_name}, {my_name}")
+print(f"Email: {first_name}.{last_name}@{company_name}.com")
+print(f"Username: {first_name}_{last_name}")
 
 
